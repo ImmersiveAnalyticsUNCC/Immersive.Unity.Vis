0e4ae7ef1fb2838ebbb7173f2a07f1a0a1311b51
luad <alu1@uncc.edu>
2019-09-04 11:31:49 -0400

    changed file reader

--- Stacked_Bars/Assets/scripts/DataAdapter.cs
+++ Stacked_Bars/Assets/scripts/DataAdapter.cs
@@ -8,22 +8,21 @@ public class DataAdaptor
     public static List<string> ReadCSVFile(string filename)
     {
         List<string> all_data = new List<string>();
-        StreamReader file = new StreamReader(filename);
-        string line;
-        header = null;
-        while ((line = file.ReadLine()) != null)
+
+        string[] reader = System.IO.File.ReadAllLines(filename);
+        if (reader == null) return null;
+
+        header = reader[0];
+
+        for (int i = 1; i < reader.Length; i++)
         {
-            if(header == null)
-            {
-                header = line;
-                continue;
-            }
-            string[] line_data = line.Split(',');
-            foreach (string data in line_data)
+            string[] line = reader[i].Split(',');
+            foreach (string data in line)
             {
                 all_data.Add(data);
             }
         }
+
         return all_data;
     }
 }
