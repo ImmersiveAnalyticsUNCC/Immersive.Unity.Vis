468a73b49c2104adfd58a056d3796b37e26cf7bf
Aidong Lu <alu1@uncc.edu>
2020-09-10 13:51:50 -0400

    add ios example

---
+++ ios/3D_BarChart/Assets/Code/Settings.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Settings {
+    public const int ENV_MAX_SIZE_HEIGHT = 5;
+    public const int ENV_MAX_SIZE_WIDTH = 10;
+    public const int ENV_MAX_SIZE_DEPTH = 10;
+
+}
