ae9b8aedfc6c08395a929f01c9785ec013d6abcd
Gadsha <gadsha@gmail.com>
2022-09-01 11:47:11 -0400

    Unity 2021

---
+++ Unity 2021/3D_barChart/Assets/Code/Movement/ManageBarEvents.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManageBarEvents : MonoBehaviour {
+
+    //public CreatePopUpLabel createPopUpLabel;
+
+	void OnMouseDown() {
+        //this piece calls the pop up creation script from the parent object
+        CreatePopUpLabel script = this.transform.parent.GetComponent<CreatePopUpLabel>();
+        script.CreateValuePopUp();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
---
+++ Unity 2021/3D_barChart/Assets/Code/Scene/CreateTitles.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateTitles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject TitlesPannel = Instantiate(Resources.Load("prefabs/others/TitlesPannel"), new Vector3(5, 7, 0), Quaternion.identity) as GameObject;
+
+        TextMesh Title = GameObject.FindWithTag("Title").GetComponent<TextMesh>();
+        Title.text = JSONtoObj.MainChart.Title;
+
+        TextMesh subTitle = GameObject.FindWithTag("Subtitle").GetComponent<TextMesh>();
+        subTitle.text = JSONtoObj.MainChart.SubTitle;
+
+        TextMesh source = GameObject.FindWithTag("DataSource").GetComponent<TextMesh>();
+        source.text = JSONtoObj.MainChart.DataSource;
+
+    }
+
+
+}
